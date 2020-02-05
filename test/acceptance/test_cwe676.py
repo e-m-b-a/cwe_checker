f20239eaef27eabf05d8e1c53154c196c4816dbd
mellowCS <cs.germany@protonmail.com>
2020-02-05 09:57:10 +0100

    changed unittest skip description

--- test/acceptance/test_cwe676.py
+++ test/acceptance/test_cwe676.py
@@ -151,22 +151,14 @@ class TestCwe676(unittest.TestCase):
             self.target, self.target, 'ppc64le', 'clang', self.string)
         self.assertEqual(res, expect_res)
 
-<<<<<<< Updated upstream
     @unittest.skip('FIXME: finds dangerous functions calls in compiler generated code.')
-=======
-    @unittest.skip('FIXME: finds potentially dangerous functions in compiler generated functions.')
->>>>>>> Stashed changes
     def test_cwe676_01_x86_mingw_gcc(self):
         expect_res = 1
         res = cwe_checker_testlib.execute_and_check_occurence(
             self.target, self.target, 'x86', 'mingw32-gcc', self.string)
         self.assertEqual(res, expect_res)
 
-<<<<<<< Updated upstream
     @unittest.skip('FIXME: finds dangerous functions calls in compiler generated code.')
-=======
-    @unittest.skip('FIXME: finds potentially dangerous functions in compiler generated functions.')
->>>>>>> Stashed changes
     def test_cwe676_01_x64_mingw_gcc(self):
         expect_res = 1
         res = cwe_checker_testlib.execute_and_check_occurence(
